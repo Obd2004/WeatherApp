63209168095470c6eec544b1660f4938ba75a918
Obd2004 <obidjonabd1097@gmail.com>
2023-03-26 16:39:18 +0500

    My Weather App

---
+++ src/WeatherApp/Main.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import Application from './Application'
+import TopNav from './TopNav'
+function Main() {
+  return (
+    <div>
+        <TopNav/>
+        <Application/>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/WeatherApp/TopNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function TopNav() {
+  return (
+    <div className='topNav'>
+        <h3>Obd Group | Weather App</h3>
+    </div>
+  )
+}
+
+export default TopNav
